c8222b1f6b0b800c85261f668176bb08da1c10bd
Lars Hummelgren <larshum@kth.se>
2025-02-08 16:40:49 +0100

    Add more parir benchmark implementations

---
+++ npbench/benchmarks/azimint_naive/azimint_naive_parir.py
@@ -0,0 +1,47 @@
+# Copyright 2014 Jérôme Kieffer et al.
+# This is an open-access article distributed under the terms of the
+# Creative Commons Attribution License, which permits unrestricted use,
+# distribution, and reproduction in any medium, provided the original author
+# and source are credited.
+# http://creativecommons.org/licenses/by/3.0/
+# Jérôme Kieffer and Giannis Ashiotis. Pyfai: a python library for
+# high performance azimuthal integration on gpu, 2014. In Proceedings of the
+# 7th European Conference on Python in Science (EuroSciPy 2014).
+
+import parir
+from parir import ParKind
+import torch
+
+@parir.jit
+def parir_kernel(data, radius, res, rmax, tmp, npt, N):
+    for ix in range(N):
+        rmax[0] = max(rmax[0], radius[ix])
+    for i in range(npt):
+        r1 = rmax[0] * parir.float64(i) / parir.float64(npt)
+        r2 = rmax[0] * parir.float64(i+1) / parir.float64(npt)
+        for j in range(N):
+            if r1 <= radius[j] and radius[j] < r2:
+                tmp[i, j] = 1.0
+            else:
+                tmp[i, j] = 0.0
+        c = 0.0
+        for jx in range(N):
+            c = c + tmp[i, jx]
+        s = 0.0
+        for jx in range(N):
+            s = s + tmp[i, jx] * data[jx]
+        res[i] = s / c
+
+def azimint_naive(data, radius, npt):
+    N, = data.shape
+    rmax = torch.empty((1,), dtype=torch.float64, device=data.device)
+    tmp = torch.empty((npt, N), dtype=torch.float64, device=data.device)
+    res = torch.zeros(npt, dtype=torch.float64, device=data.device)
+    p = {
+        'i': [ParKind.GpuThreads(npt)],
+        'ix': [ParKind.GpuThreads(1024), ParKind.GpuReduction()],
+        'j': [ParKind.GpuThreads(1024)],
+        'jx': [ParKind.GpuThreads(1024), ParKind.GpuReduction()]
+    }
+    parir_kernel(data, radius, res, rmax, tmp, npt, N, parallelize=p)
+    return res
---
+++ npbench/benchmarks/polybench/deriche/deriche_parir.py
@@ -0,0 +1,71 @@
+import parir
+from parir import ParKind
+import torch
+
+@parir.jit
+def parir_kernel(a, b, c, imgIn, imgOut, y1, y2, W, H):
+    for i in range(W):
+        y1[i, 0] = a[0] * imgIn[i, 0]
+        y1[i, 1] = a[0] * imgIn[i, 1] + a[1] * imgIn[i, 0] + b[0] * y1[i, 0]
+    for jx in range(2, H):
+        for i in range(W):
+            y1[i, jx] = (a[0] * imgIn[i, jx] + a[1] * imgIn[i, jx-1] +
+                         b[0] * y1[i, jx-1] + b[1] * y1[i, jx-2])
+
+    for i in range(W):
+        y2[i, H-1] = 0.0
+        y2[i, H-2] = a[2] * imgIn[i, H-1]
+    for jx in range(0, H-2):
+        for i in range(W):
+            j = (H-3) - jx
+            y2[i, j] = (a[2] * imgIn[i, j+1] + a[3] * imgIn[i, j+2] +
+                        b[0] * y2[i, j+1] + b[1] * y2[i, j+2])
+
+    for i in range(W):
+        for j in range(H):
+            imgOut[i, j] = c[0] * (y1[i, j] + y2[i, j])
+
+    for j in range(H):
+        y1[0, j] = a[4] * imgOut[0, j]
+        y1[1, j] = a[4] * imgOut[1, j] + a[5] * imgOut[0, j] + b[0] * y1[0, j]
+    for ix in range(2, W):
+        for j in range(H):
+            y1[ix, j] = (a[4] * imgOut[ix, j] + a[5] * imgOut[ix-1, j] +
+                         b[0] * y1[ix-1, j] + b[1] * y1[ix-2, j])
+
+    for j in range(H):
+        y2[W-1, j] = 0.0
+        y2[W-2, j] = a[6] * imgOut[W-1, j]
+    for ix in range(0, W-2):
+        for j in range(H):
+            i = (W-3) - ix
+            y2[i, j] = (a[6] * imgOut[i+1, j] + a[7] * imgOut[i+2, j] +
+                        b[0] * y2[i+1, j] + b[1] * y2[i+2, j])
+
+    for i in range(W):
+        for j in range(H):
+            imgOut[i, j] = c[1] * (y1[i, j] + y2[i, j])
+
+def kernel(alpha, imgIn):
+    y1 = torch.empty_like(imgIn)
+    y2 = torch.empty_like(imgIn)
+    imgOut = torch.empty_like(imgIn)
+    W, H = imgIn.shape
+    k = (1.0 - parir.exp(-alpha)) * (1.0 - parir.exp(-alpha)) / (
+        1.0 + alpha * parir.exp(-alpha) - parir.exp(2.0 * alpha))
+    a = torch.empty(8, dtype=y1.dtype, device=y1.device)
+    b = torch.empty(2, dtype=y1.dtype, device=y1.device)
+    c = torch.empty_like(b)
+    a[0] = a[4] = k
+    a[1] = a[5] = k * parir.exp(-alpha) * (alpha - 1.0)
+    a[2] = a[6] = k * parir.exp(-alpha) * (alpha + 1.0)
+    a[3] = a[7] = -k * parir.exp(-2.0 * alpha)
+    b[0] = 2.0**(-alpha)
+    b[1] = -parir.exp(-2.0 * alpha)
+    c[0] = c[1] = 1.0
+    p = {
+        'i': [ParKind.GpuThreads(W)],
+        'j': [ParKind.GpuThreads(H)]
+    }
+    parir_kernel(a, b, c, imgIn, imgOut, y1, y2, W, H, parallelize=p)
+    return imgOut
