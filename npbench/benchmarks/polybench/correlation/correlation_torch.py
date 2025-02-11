ffe0ffc86493c2aa332f3f1350ca69956a0b14e2
Lars Hummelgren <larshum@kth.se>
2025-02-11 13:15:57 +0100

    Remove PyTorch workaround via NumPy for consistency

--- npbench/benchmarks/polybench/correlation/correlation_torch.py
+++ npbench/benchmarks/polybench/correlation/correlation_torch.py
@@ -5,8 +5,7 @@ import torch
 def kernel(M, float_n, data):
     float_n = torch.tensor(float_n, dtype=torch.float64, device=data.device)
     mean = torch.mean(data, axis=0)
-    # NOTE: Translating to NumPy and back for precision as torch.std fails validation
-    stddev = torch.tensor(np.std(data.cpu().numpy(), axis=0), dtype=torch.float64, device=data.device)
+    stddev = torch.std(data, axis=0)
     stddev[stddev <= 0.1] = 1.0
     data -= mean
     data /= torch.sqrt(float_n) * stddev
