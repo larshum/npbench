efdcb75885a47db8400022de3d009055ccd8130f
Lars Hummelgren <larshum@kth.se>
2025-02-15 14:39:18 +0100

    Add Parir implementations of three more benchmarks

---
+++ npbench/benchmarks/polybench/symm/symm_parir.py
@@ -0,0 +1,35 @@
+import parir
+import torch
+
+
+@parir.jit
+def parir_kernel(alpha, beta, C, A, B, M, N):
+    parir.label('M')
+    for i in range(M):
+        parir.label('N')
+        for j in range(N):
+            C[i,j] *= beta
+
+    for i in range(M):
+        parir.label('N')
+        for j in range(N):
+            parir.label('M')
+            for k in range(i):
+                C[k,j] += alpha * B[i,j] * A[i,k]
+
+        parir.label('N')
+        for j in range(N):
+            dot_sum = 0.0
+            parir.label('i_red')
+            for k in range(i):
+                dot_sum += B[k,j] * A[i,k]
+            C[i,j] += alpha * B[i,j] * A[i,i] + alpha * dot_sum
+
+def kernel(alpha, beta, C, A, B):
+    M, N = C.shape
+    p = {
+        'M': [parir.threads(M)],
+        'N': [parir.threads(N)],
+        'i_red': [parir.threads(32), parir.reduce()]
+    }
+    parir_kernel(alpha, beta, C, A, B, M, N, parallelize=p)
---
+++ npbench/benchmarks/polybench/trisolv/trisolv_parir.py
@@ -0,0 +1,21 @@
+import parir
+import torch
+
+
+@parir.jit
+def parir_kernel(L, x, b, N):
+    parir.label('N')
+    for i in range(N):
+        t = 0.0
+        parir.label('reduce')
+        for k in range(i):
+            t += L[i,k] * x[k]
+        x[i] = (b[i] - t) / L[i,i]
+
+def kernel(L, x, b):
+    N = x.shape[0]
+    p = {
+        'N': [parir.threads(N)],
+        'reduce': [parir.threads(32), parir.reduce()]
+    }
+    parir_kernel(L, x, b, N, parallelize=p)
