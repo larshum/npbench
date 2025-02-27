5e1ffe0de20947d35d9e17e94ede9e9c397f0f77
Lars Hummelgren <larshum@kth.se>
2025-02-27 22:26:28 +0100

    Add more Parir implementations

---
+++ npbench/benchmarks/polybench/correlation/correlation_parir.py
@@ -0,0 +1,29 @@
+import parir
+import torch
+
+
+@parir.jit
+def parir_kernel(corr, data, M):
+    parir.label('i')
+    for i in range(M-1):
+        parir.label('j')
+        for j in range(i+1, M):
+            parir.label('reduce')
+            corr[i, j] = parir.sum(data[:, i] * data[:, j], axis=0)
+            corr[j, i] = corr[i, j]
+
+def kernel(M, float_n, data):
+    float_n = torch.tensor(float(float_n), dtype=torch.float64, device=data.device)
+    mean = torch.mean(data, axis=0)
+    stddev = torch.std(data, unbiased=False, axis=0)
+    stddev[stddev <= 0.1] = 1.0
+    data -= mean
+    data /= torch.sqrt(float_n) * stddev
+    corr = torch.eye(M, dtype=data.dtype, device=data.device)
+    p = {
+        'i': [parir.threads(M-1)],
+        'j': [parir.threads(M//2)],
+        'reduce': [parir.threads(1024)]
+    }
+    parir_kernel(corr, data, M, parallelize=p)
+    return corr
---
+++ npbench/benchmarks/polybench/covariance/covariance_parir.py
@@ -0,0 +1,27 @@
+import parir
+import torch
+
+
+@parir.jit
+def covariance_parir(cov, data, float_n, M):
+    parir.label('i')
+    for i in range(M):
+        parir.label('j')
+        for j in range(i, M):
+            parir.label('reduce')
+            s = parir.sum(data[:, i] * data[:, j], axis=0)
+            cov[i, j] = s / (float_n - 1.0)
+            cov[j, i] = cov[i, j]
+
+def kernel(M, float_n, data):
+    float_n = torch.tensor(float(float_n), dtype=data.dtype, device=data.device)
+    mean = torch.mean(data, axis=0)
+    data -= mean
+    cov = torch.zeros((M, M), dtype=data.dtype, device=data.device)
+    p = {
+        'i': [parir.threads(M)],
+        'j': [parir.threads(M//2)],
+        'reduce': [parir.threads(1024)]
+    }
+    covariance_parir(cov, data, float_n, M, parallelize=p)
+    return cov
