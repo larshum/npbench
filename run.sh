ad6263c38a7bb53c44b45cd8f4fe3a975824af7b
Lars Hummelgren <larshum@kth.se>
2025-03-27 16:07:27 +0100

    Add script for running our benchmarks

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+OUT="out/"
+frameworks="cupy dace_gpu jax parir torch_gpu"
+
+rm -f npbench.db
+mkdir -p ${OUT}
+
+for f in $frameworks; do
+  echo "Running benchmarks for framework $f"
+  python run_framework.py -f $f -p L > ${OUT}/$f.out 2> ${OUT}/$f.err
+done
+
+python plot_results.py -p L -b cupy
