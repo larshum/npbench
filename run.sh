8823c7963ac317d10971c44425b582c4f9e8e435
Lars Hummelgren <larshum@kth.se>
2025-06-18 20:11:00 +0200

    Use a fine-grained approach to running benchmarks

--- run.sh
+++ run.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
 OUT="out/"
-frameworks="cupy dace_gpu jax parir torch_gpu"
+FRAMEWORKS=("cupy" "dace_gpu" "jax" "parir" "torch_gpu")
+BENCHMARKS=("adi" "arc_distance" "azimint_naive" "cavity_flow" "channel_flow" "cholesky" "compute" "conv2d_bias" "correlation" "covariance" "crc16" "deriche" "durbin" "fdtd_2d" "floyd_warshall" "go_fast" "gramschmidt" "hdiff" "heat_3d" "jacobi_1d" "jacobi_2d" "lenet" "lu" "ludcmp" "mlp" "nbody" "nussinov" "resnet" "scattering_self_energies" "seidel_2d" "softmax" "spmv" "symm" "syr2k" "syrk" "trisolv" "trmm" "vadv")
+PRESET=L
 
 rm -f npbench.db
+rm -f ${OUT}/*.out ${OUT}/*.err
 mkdir -p ${OUT}
 
-for f in $frameworks; do
-  echo "Running benchmarks for framework $f"
-  python run_framework.py -f $f -p L > ${OUT}/$f.out 2> ${OUT}/$f.err
-done
+echo "Running ${#BENCHMARKS[@]} benchmarks using ${#FRAMEWORKS[@]} frameworks"
 
-python plot_results.py -p L -b cupy
+for f in ${FRAMEWORKS[@]}; do
+  echo "Benchmarking framework $f"
+  for b in ${BENCHMARKS[@]}; do
+    python run_benchmark.py -f $f -b $b -p $PRESET >> ${OUT}/$f.out 2>> ${OUT}/$f.err
+  done
+done
+python plot_results.py -p $PRESET -b torch_gpu
