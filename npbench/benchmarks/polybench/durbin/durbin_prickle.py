2bf49493af05bd65fc952976c1aee48e753b8f2a
Lars Hummelgren <larshum@kth.se>
2025-08-12 16:11:34 +0200

    Remove dead code in durbin for Prickle

--- npbench/benchmarks/polybench/durbin/durbin_prickle.py
+++ npbench/benchmarks/polybench/durbin/durbin_prickle.py
@@ -34,15 +34,3 @@ def kernel(r):
     }
     prickle_kernel(r, y, temp, N, opts=prickle.par(p))
     return y
-
-    alpha = -r[0]
-    beta = 1.0
-    y[0] = -r[0]
-
-    for k in range(1, r.shape[0]):
-        beta *= 1.0 - alpha * alpha
-        alpha = -(r[k] + torch.dot(torch.flip(r[:k], dims=[0]), y[:k])) / beta
-        y[:k] += alpha * torch.flip(y[:k], dims=[0])
-        y[k] = alpha
-
-    return y
