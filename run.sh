9ab7e879ad1254be1f59ae9eb7d27b565014fca2
Lars Hummelgren <larshum@kth.se>
2026-01-13 09:10:58 +0100

    Remove gemm benchmark

--- run.sh
+++ run.sh
@@ -28,7 +28,7 @@ function run_benchmarks {
 
 function bench_cuda {
   export FRAMEWORKS=("cupy" "dace_gpu" "jax" "parpy_cuda" "torch_cuda")
-  export BENCHMARKS=("adi" "arc_distance" "azimint_naive" "cavity_flow" "channel_flow" "cholesky" "compute" "conv2d_bias" "correlation" "covariance" "crc16" "deriche" "durbin" "fdtd_2d" "floyd_warshall" "gemm" "go_fast" "gramschmidt" "hdiff" "heat_3d" "jacobi_1d" "jacobi_2d" "lenet" "lu" "ludcmp" "nbody" "nussinov" "resnet" "scattering_self_energies" "seidel_2d" "softmax" "spmv" "symm" "syr2k" "syrk" "trisolv" "trmm" "vadv")
+  export BENCHMARKS=("adi" "arc_distance" "azimint_naive" "cavity_flow" "channel_flow" "cholesky" "compute" "conv2d_bias" "correlation" "covariance" "crc16" "deriche" "durbin" "fdtd_2d" "floyd_warshall" "go_fast" "gramschmidt" "hdiff" "heat_3d" "jacobi_1d" "jacobi_2d" "lenet" "lu" "ludcmp" "nbody" "nussinov" "resnet" "scattering_self_energies" "seidel_2d" "softmax" "spmv" "symm" "syr2k" "syrk" "trisolv" "trmm" "vadv")
   export PRESET=L
   export BASELINE=torch_cuda
   export VALIDATOR=numpy
