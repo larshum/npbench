fd96235649fa5a58fa64f1216fc0ed583339e24d
Lars Hummelgren <larshum@kth.se>
2026-01-13 09:09:51 +0100

    Optimize the conv2d_bias function

--- npbench/benchmarks/deep_learning/conv2d_bias/conv2d_parpy.py
+++ npbench/benchmarks/deep_learning/conv2d_bias/conv2d_parpy.py
@@ -4,15 +4,16 @@ import torch
 
 @parpy.jit
 def conv2d_kernel(inputs, weights, output, H_out, W_out, N, C_in, C_out, K):
-    parpy.label('i')
+    parpy.label('H_out')
     for i in range(H_out):
-        parpy.label('j')
+        parpy.label('W_out')
         for j in range(W_out):
             output[:,i,j,:] = 0.0
             for a in range(N):
                 for b in range(K):
                     for c in range(K):
                         for d in range(C_in):
+                            parpy.label('C_out')
                             for e in range(C_out):
                                 output[a,i,j,e] += inputs[a,i+b,j+c,d] * weights[b,c,d,e]
 
@@ -33,7 +34,11 @@ def conv2d_bias(input, weights, bias):
     C_in = input.shape[3]
     C_out = weights.shape[3]
     output = parpy.buffer.empty((N, H_out, W_out, C_out), parpy.types.F32, weights.backend())
-    p = {'i': parpy.threads(H_out), 'j': parpy.threads(W_out)}
+    p = {
+        'H_out': parpy.threads(H_out),
+        'W_out': parpy.threads(W_out),
+        'C_out': parpy.threads(C_out)
+    }
     conv2d_kernel(input, weights, output, H_out, W_out, N, C_in, C_out, K, opts=parpy.par(p))
     p = {'i': parpy.threads(C_out), 'j': parpy.threads(N), 'k': parpy.threads(H_out), 'l': parpy.threads(W_out)}
     add_elemwise(output, bias, C_out, opts=parpy.par(p))
